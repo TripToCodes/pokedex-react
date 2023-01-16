61822f7be6bef84d0b631a39976ecaa8681cec60
TripToCodes <iam.somj@gmail.com>
2023-01-16 09:54:52 -0800

    [Fix] Improve the format of pokemon stats

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,22 +4,11 @@ import CardFrontSide from "../CardFrontSide/CardFrontSide";
 import styles from "./Card.module.css";
 
 export default function Card({ pokemon, onCollect }) {
-  // const [flipped, setFlipped] = useState(false);
-
   return (
     <div className={styles.card}>
       <div className={styles.card__content}>
-        <CardBackSide
-          pokemon={pokemon}
-          // cardStatus={flipped}
-          // onFlipChange={(cardStatus) => setFlipped(cardStatus)}
-          onCollect={onCollect}
-        />
-        <CardFrontSide
-          pokemon={pokemon}
-          // cardStatus={flipped}
-          // onFlipChange={(cardStatus) => setFlipped(cardStatus)}
-        />
+        <CardBackSide pokemon={pokemon} onCollect={onCollect} />
+        <CardFrontSide pokemon={pokemon} />
       </div>
     </div>
   );
--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Stats({ statName, statNumber }) {
-  return <div>{}</div>;
+export default function Stats({ pokemonStats }) {
+  return (
+    <>
+      <div>Stats:</div>
+      {pokemonStats.map((stat) => {
+        return <div key={stat.name}>{`${stat.name}: ${stat.base_stat}`}</div>;
+      })}
+    </>
+  );
 }
